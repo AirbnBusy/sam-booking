a538c631f397be21eaa335d36eabae919d782d7c
Sam Rowland <srowland2594@gmail.com>
2018-06-01 09:57:54 -0700

    Changed from Airbnb-base linter to Airbnb, cleaned up syntax for basic react app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      Hello from React!
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    Hello from React!
+  </div>
+);
+
 
 export default App;
