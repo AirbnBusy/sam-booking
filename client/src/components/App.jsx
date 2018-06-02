46d41bd47ec601b199eb8a8a8ea904eaf0aec909
Sam Rowland <srowland2594@gmail.com>
2018-06-01 19:09:45 -0700

    Finished basic routes to the server on initialization of the app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <div>
-    Hello from React!
-  </div>
-);
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listingId: 1001,
+    };
+  }
+
+  componentDidMount() {
+    this.getListing();
+  }
+
+  getListing() {
+    axios.get(`http://localhost:3001/api/listings/${this.state.listingId}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.listingId}
+      </div>
+    );
+  }
+}
 
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/listings/:userId', express.static(path.join(__dirname, '../client/dist')))
+app.use(bodyParser.json());
+
+app.get('/api/listings/:userId', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/api/listings/:userId/calendar/:yearMonth', (req, res) => {
+  res.send(req.params);
+});
 
 module.exports = app;
