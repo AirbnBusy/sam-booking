d6921af9cbb4ef0550b8ee896b82971ed7cc19f3
Sam Rowland <srowland2594@gmail.com>
2018-06-04 14:30:15 -0700

    Added dateHelpers to get special fields for Calendar rendering, Created Calendar Component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Calendar from './Calendar.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,11 +8,17 @@ class App extends React.Component {
 
     const date = new Date();
     const year = date.getUTCFullYear().toString();
-    const month = date.getUTCMonth() < 10 ? '0' + date.getUTCMonth().toString() : date.getUTCMonth().toString();
+    const month = date.getUTCMonth() < 10 ? date.getUTCMonth().toString() : date.getUTCMonth().toString();
   
     this.state = {
       listingId: 1001,
       yearMonth: year + month,
+      baseRate: null,
+      cleaningFee: null,
+      maxGuests: null,
+      currentCalendarDatesUnavailable: [],
+      currentMonth: null,
+      currentYear: null,
     };
   }
 
@@ -25,27 +32,37 @@ class App extends React.Component {
       .then((res) => {
         const { data } = res;
         this.setState({
-          listingId: data.listingId,
+          listingId: data.id,
+          baseRate: data.base_rate_per_night,
+          cleaningFee: data.cleaning_fee,
+          maxGuests: data.max_guests,
         });
       })
       .catch((err) => {
-        console.log(err);
+        console.log('ERROR GETTING LISTING DATA: ', err);
       });
   }
 
   getCalendar() {
     axios.get(`http://localhost:3001/api/listings/${this.state.listingId}/calendar/${this.state.yearMonth}`)
       .then((res) => {
-        console.log(res)
+        const { data } = res;
+        console.log('DATA: ', data);
+        this.setState({
+          currentCalendarDatesUnavailable: data.days,
+          currentMonth: data.month,
+          currentYear: data.year,
+        });
       })
       .catch((err) => {
-        console.log(err);
+        console.log('ERROR GETTING CALENDAR DATA: ', err);
       });
   }
 
   render() {
     return (
       <div>
+        <Calendar daysUnav={this.state.currentCalendarDatesUnavailable}/>
         {this.state.listingId}
       </div>
     );
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('../database');
+const dateHelpers = require('./dateHelpers');
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.get('/listings/:listingId', (req, res) => {
 app.get('/api/listings/:listingId', (req, res) => {
   db.getListingInfo(req.params.listingId)
     .then((rows) => {
-      console.log(rows);
+      res.send(rows[0]);
     })
     .catch((err) => {
       res.statusCode(500).send(err);
@@ -23,14 +24,30 @@ app.get('/api/listings/:listingId', (req, res) => {
 });
 
 app.get('/api/listings/:listingId/calendar/:yearMonth', (req, res) => {
-  db.getUnavailableDates(req.params.listingId, req.params.yearMonth)
+  const month = req.params.yearMonth.substring(4);
+  const year = req.params.yearMonth.substring(0, 4);
+
+  // Get special fields from dateHelpers
+  const firstDayOfMonth = dateHelpers.getPositionOfFirstDayOfMonth(year, month);
+  const numberOfDaysInMonth = dateHelpers.getNumberOfDaysInMonth(year, month);
+  const monthName = dateHelpers.getMonthName(month);
+
+  // SQL Formatting
+  const sqlMonthNum = Number(month) + 1;
+  const sqlMonth = sqlMonthNum < 10 ? '0' + sqlMonthNum.toString() : sqlMonthNum.toString();
+  const sqlYearMonth = year + sqlMonth;
+
+  db.getUnavailableDates(req.params.listingId, sqlYearMonth)
     .then((rows) => {
       const days = rows.map(row => row.day);
       const responseObj = {
-        days: days,
+        days,
+        monthName,
+        numberOfDaysInMonth,
+        firstDayOfMonth,
+        year,
         listingId: req.params.listingId,
       };
-      console.log(responseObj);
       res.send(responseObj);
     })
     .catch((err) => {
